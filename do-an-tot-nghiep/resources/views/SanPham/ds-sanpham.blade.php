b11a67f1c3ec69e99f4926f2b15d0c5d895cb1a0
Le Hong Lam Truong <55741716+lamtruongxxy@users.noreply.github.com>
2020-07-18 21:50:00 +0700

    update template

--- do-an-tot-nghiep/resources/views/SanPham/ds-sanpham.blade.php
+++ do-an-tot-nghiep/resources/views/SanPham/ds-sanpham.blade.php
@@ -15,12 +15,11 @@
 @endsection
 
 @section('main-content')
-<div class="container-fluid">
+{{-- <div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-2 text-gray-800">Danh sách sản phẩm</h1>
-    <p class="mb-4">DataTables is a third party plugin that is used to gen</a>.</p>
-
+    <h1 class="h6 mb-2 text-gray-800">Home / Sản phẩm / Danh sách sản phẩm</h1>
+    
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
@@ -36,33 +35,42 @@
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
-                            <th>Name</th>
-                            <th>Position</th>
-                            <th>Office</th>
-                            <th>Age</th>
-                            <th>Start date</th>
-                            <th>Salary</th>
+                            <th>Mã sản phẩm</th>
+                            <th>Tên sản phẩm</th>
+                            <th>Loại sản phẩm</th>
+                            <th>Nhà sản xuất</th>
+                            <th>Mô tả</th>
+                            <th>Số lượng tồn kho</th>
+                            <th>Chế độ bảo hành</th>
+                            <th>Trạng thái</th>
+                            <th>Thông số</th>
                             <th>Thao tác</th>
                         </tr>
                     </thead>
                     <tfoot>
                         <tr>
-                            <th>Name</th>
-                            <th>Position</th>
-                            <th>Office</th>
-                            <th>Age</th>
-                            <th>Start date</th>
-                            <th>Salary</th>
+                            <th>Mã sản phẩm</th>
+                            <th>Tên sản phẩm</th>
+                            <th>Loại sản phẩm</th>
+                            <th>Nhà sản xuất</th>
+                            <th>Mô tả</th>
+                            <th>Số lượng tồn kho</th>
+                            <th>Chế độ bảo hành</th>
+                            <th>Trạng thái</th>
+                            <th>Thông số</th>
                             <th>Thao tác</th>
                         </tr>
                     </tfoot>
                     <tbody>
                         <tr>
-                            <td>Tiger </td>
-                            <td>System </td>
-                            <td>Edinburgh</td>
-                            <td>61</td>
-                            <td>201</td>
+                            <td>1</td>
+                            <td>a </td>
+                            <td>b</td>
+                            <td>c</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
                             <td>$320</td>
                             <td>
                                 <a href="#" class="btn btn-success btn-icon-split">
@@ -75,10 +83,160 @@
                                 </a>
                             </td>
                         </tr>
+                        
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
+</div> --}}
+<div class="wrapper">
+    <div class="container-fluid">
+
+        <!-- start page title -->
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box">
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="#">Sản phẩm</a></li>
+                            <li class="breadcrumb-item active">Danh sách sản phẩm</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>     
+        <!-- end page title --> 
+
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="widget-body-toolbar margin-top-10 form-horizontal">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <div class="form-group">
+                                        <label class="col-md-8 control-label">Nhóm sản phẩm</label>
+                                        <div class="col-md-8">
+                                            <select class="form-control" id="ProductGroupId" name="ProductGroupId"><option value="0">Toàn bộ danh mục</option>
+                                            <option value="220796">Điện thoại</option>
+                                            <option value="220804">├──Apple</option>
+                                            <option value="220805">├──Nokia</option>
+                                            <option value="220803">├──Samsung</option>
+                                            <option value="220802">├──Sony</option>
+                                            <option value="220797">Máy tính bảng</option>
+                                            <option value="220798">Laptop</option>
+                                            <option value="220799">Máy để bàn</option>
+                                            <option value="220800">Phụ kiện</option>
+                                            <option value="220801">Máy cũ</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="form-group">
+                                        <label class="col-md-8 control-label">Tag sản phẩm</label>
+                                        <div class="col-md-8">
+                                            <select class="form-control" id="ProductTag" name="ProductTag">
+                                                <option value="0">----------Tất cả----------</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" id="txtKeyword">
+                                        <div class="input-group-btn">
+                                            <button type="button" class="btn btn-default" id="btnSearch">
+                                                <i class="fa fa-search"></i> Tìm
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <table id="basic-datatable" class="table dt-responsive nowrap">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Position</th>
+                                    <th>Office</th>
+                                    <th>Age</th>
+                                    <th>Start date</th>
+                                    <th>Salary</th>
+                                </tr>
+                            </thead>
+                        
+                        
+                            <tbody>
+                                <tr>
+                                    <td>Tiger Nixon</td>
+                                    <td>System Architect</td>
+                                    <td>Edinburgh</td>
+                                    <td>61</td>
+                                    <td>2011/04/25</td>
+                                    <td>$320,800</td>
+                                </tr>
+                                <tr>
+                                    <td>Garrett Winters</td>
+                                    <td>Accountant</td>
+                                    <td>Tokyo</td>
+                                    <td>63</td>
+                                    <td>2011/07/25</td>
+                                    <td>$170,750</td>
+                                </tr>
+                                <tr>
+                                    <td>Ashton Cox</td>
+                                    <td>Junior Technical Author</td>
+                                    <td>San Francisco</td>
+                                    <td>66</td>
+                                    <td>2009/01/12</td>
+                                    <td>$86,000</td>
+                                </tr>
+                                <tr>
+                                    <td>Cedric Kelly</td>
+                                    <td>Senior Javascript Developer</td>
+                                    <td>Edinburgh</td>
+                                    <td>22</td>
+                                    <td>2012/03/29</td>
+                                    <td>$433,060</td>
+                                </tr>
+                                <tr>
+                                    <td>Airi Satou</td>
+                                    <td>Accountant</td>
+                                    <td>Tokyo</td>
+                                    <td>33</td>
+                                    <td>2008/11/28</td>
+                                    <td>$162,700</td>
+                                </tr>
+                                <tr>
+                                    <td>Brielle Williamson</td>
+                                    <td>Integration Specialist</td>
+                                    <td>New York</td>
+                                    <td>61</td>
+                                    <td>2012/12/02</td>
+                                    <td>$372,000</td>
+                                </tr>
+                                <tr>
+                                    <td>Herrod Chandler</td>
+                                    <td>Sales Assistant</td>
+                                    <td>San Francisco</td>
+                                    <td>59</td>
+                                    <td>2012/08/06</td>
+                                    <td>$137,500</td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+
+                    </div> <!-- end card body-->
+                </div> <!-- end card -->
+            </div><!-- end col-->
+        </div>
+        <!-- end row-->
+ 
+    </div> <!-- end container -->
 </div>
 @endsection
--- do-an-tot-nghiep/resources/views/master-page.blade.php
+++ do-an-tot-nghiep/resources/views/master-page.blade.php
@@ -2,14 +2,13 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Blank</title>
+    <title>TTmobile - Admin</title>
     <!-- Custom fonts for this template-->
     <link href="{{ asset ('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -32,9 +31,9 @@
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+                    <i class="fas fa-home"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                <div class="sidebar-brand-text mx-3">TTmobile Admin </div>
             </a>
 
             <!-- Divider -->
@@ -42,91 +41,85 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
+                    <span>Tổng Quan</span></a>
             </li>
 
             <!-- Divider -->
             <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Interface
-            </div>
-
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
-                </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
-                    </div>
-                </div>
-            </li>
-
-            <!-- Nav Item - Utilities Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
-                </a>
-                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="utilities-color.html">Colors</a>
-                        <a class="collapse-item" href="utilities-border.html">Borders</a>
-                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                        <a class="collapse-item" href="utilities-other.html">Other</a>
-                    </div>
-                </div>
-            </li>
-
             <!-- Divider: viền ngang ngăn cách -->
-            <hr class="sidebar-divider">
-
             <!-- Heading -->
             <!-- <div class="sidebar-heading">
                 Addons
             </div> -->
 
-            <!-- Nav Item - Pages Collapse Menu -->
+            <!-- Nav Item - sản phẩm -->
             <li class="nav-item">
-                <a class="nav-link collapsed" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-house-damage"></i>
-                    <span>Quản lý sản phẩm</span>
+                <a class="nav-link collapsed" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <i class="fas fa-layer-group"></i>
+                    <span>Sản Phẩm</span>
                 </a>
-                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                <div id="collapseOne" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="{{ route('san-pham.danh-sach') }}">Danh sách sản phẩm</a>
-                        <!-- <a class="collapse-item" href="register.html">Register</a>
-                        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                        <div class="collapse-divider"></div>
-                        
-                        <a class="collapse-item" href="404.html">404 Page</a>
-                        <a class="collapse-item" href="blank.html">Blank Page</a> -->
+                        <a class="collapse-item" href="{{ route('san-pham.danh-sach') }}">Danh Sách Sản phẩm</a>
+                        <a class="collapse-item" href="#">Loại Sản Phẩm</a>
+                        <a class="collapse-item" href="#">Nhà Sản Xuất</a>
                     </div>
                 </div>
             </li>
 
-            <!-- Nav Item - Charts -->
+            <!-- Nav Item - tin tức -->
             <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
+                <a class="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fa fa-fw fa-table"></i>
+                    <span>Tin Tức</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="#">Thêm Mới Tin Tức</a>
+                        <a class="collapse-item" href="#">Danh Sách Tin Tức</a>
+                        <a class="collapse-item" href="#">Nhóm Tin Tức</a>
+                    </div>
+                </div>
             </li>
+            <!-- Nav Item - đơn hàng -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                    <i class="fas fa-cart-plus"></i>
+                    <span>Đơn Hàng</span>
+                </a>
+                <div id="collapseThree" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="#">Thêm Mới Đơn Hàng</a>
+                        <a class="collapse-item" href="#">Danh Sách Đơn Hàng</a>
+                        <a class="collapse-item" href="#">Hóa Đơn</a>
+                    </div>
+                </div>
+            </li>
+             <!-- Nav Item - Khách Hàng -->
+             <li class="nav-item">
+                <a class="nav-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+                    <i class="fas fa-user"></i>
+                    <span>Khách Hàng</span>
+                </a>
+                <div id="collapseFour" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="#">Thêm Mới Khách Hàng</a>
+                        <a class="collapse-item" href="#">Danh Sách Khách Hàng</a>
+                        <a class="collapse-item" href="#">Nhóm Tin Tức</a>
+                    </div>
+                </div>
+            </li>
+            <!-- Nav Item - Bình Luận- phản hồi -->
+      <li class="nav-item">
+        <a class="nav-link" href="">
+          <i class="fas fa-bullhorn"></i>
+          <span>Bình Luận - Phản Hồi</span></a>
+      </li>
 
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
-            </li>
+
+            
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
@@ -152,19 +145,7 @@
                     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                         <i class="fa fa-bars"></i>
                     </button>
-
-                    <!-- Topbar Search -->
-                    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-
+                    
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
 
