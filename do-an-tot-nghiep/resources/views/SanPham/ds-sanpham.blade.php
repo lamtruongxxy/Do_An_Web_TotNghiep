e09b540416f1a6d11381dd8fb712bd5a54dc5b61
anhtu04091999 <55349397+anhtu04091999@users.noreply.github.com>
2020-07-21 11:30:18 +0700

    update button

--- do-an-tot-nghiep/resources/views/SanPham/ds-sanpham.blade.php
+++ do-an-tot-nghiep/resources/views/SanPham/ds-sanpham.blade.php
@@ -2,82 +2,83 @@
 @extends('master-page')
 
 @section('css')
- <!-- third party css -->
- <link href="{{ asset('assets/libs/datatables/dataTables.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('assets/libs/datatables/responsive.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('assets/libs/datatables/buttons.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('assets/libs/datatables/select.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
-        <!-- third party css end -->
+<!-- third party css -->
+<link href="{{ asset('assets/libs/datatables/dataTables.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/libs/datatables/responsive.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/libs/datatables/buttons.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/libs/datatables/select.bootstrap4.css') }}" rel="stylesheet" type="text/css" />
+<!-- third party css end -->
 @endsection
 
 @section('js')
-  <!-- third party js -->
-        <script src="{{ asset ('assets/libs/datatables/jquery.dataTables.min.js') }}"></script>
-        <script src="{{ asset ('assets/libs/datatables/dataTables.bootstrap4.js') }}"></script>
-        <script src="{{ asset ('assets/libs/datatables/dataTables.responsive.min.js') }}"></script>
-        <script src="{{ asset ('assets/libs/datatables/responsive.bootstrap4.min.js') }}"></script>
-        <script src="{{ asset ('assets/libs/datatables/dataTables.buttons.min.js') }}"></script>
-        <script src="{{ asset ('assets/libs/datatables/buttons.bootstrap4.min.js') }}"></script>
-        <script src="{{ asset ('assets/libs/datatables/buttons.html5.min.js') }}"></script>
-        <script src="{{ asset ('assets/libs/datatables/buttons.flash.min.js') }}"></script>
-        <script src="{{ asset ('assets/libs/datatables/buttons.print.min.js') }}"></script>
-        <script src="{{ asset ('assets/libs/datatables/dataTables.keyTable.min.js') }}"></script>
-        <script src="{{ asset ('assets/libs/datatables/dataTables.select.min.js') }}"></script>
-        <script src="{{ asset ('assets/libs/pdfmake/pdfmake.min.js') }}"></script>
-        <script src="{{ asset ('assets/libs/pdfmake/vfs_fonts.js') }}"></script>
-        <!-- third party js ends -->
-        <!-- Datatables init -->
-        <script src="{{ asset ('assets/js/pages/datatables.init.js') }}"></script> 
-        <!-- truy van database -->
-        <script>
-            $('#product-table').DataTable({
-                processing: true,
-                serverSide: true,
-                ajax: "{{ route('san-pham.lay-danh-sach') }}",
-                order: [],
-                columns: [{
-                    data: 'id',
-                    name:'ID Sản phẩm'
-                },{
-                    data: 'ten_sp',
-                    name:'Tên sản phẩm'
-                }, {
-                    data: 'mo_ta_sp',
-                    name:'Mô tả sản phẩm'
-                }, {
-                    data: 'loai_san_pham.ten_loai_sp',
-                    name: 'Loại sản phẩm'
-                }, {
-                    data: 'action',
-                    name: 'action'
-                }]
-            })
-        
-        </script>
+<!-- third party js -->
+<script src="{{ asset ('assets/libs/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset ('assets/libs/datatables/dataTables.bootstrap4.js') }}"></script>
+<script src="{{ asset ('assets/libs/datatables/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset ('assets/libs/datatables/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset ('assets/libs/datatables/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset ('assets/libs/datatables/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset ('assets/libs/datatables/buttons.html5.min.js') }}"></script>
+<script src="{{ asset ('assets/libs/datatables/buttons.flash.min.js') }}"></script>
+<script src="{{ asset ('assets/libs/datatables/buttons.print.min.js') }}"></script>
+<script src="{{ asset ('assets/libs/datatables/dataTables.keyTable.min.js') }}"></script>
+<script src="{{ asset ('assets/libs/datatables/dataTables.select.min.js') }}"></script>
+<script src="{{ asset ('assets/libs/pdfmake/pdfmake.min.js') }}"></script>
+<script src="{{ asset ('assets/libs/pdfmake/vfs_fonts.js') }}"></script>
+<!-- third party js ends -->
+<!-- Datatables init -->
+<script src="{{ asset ('assets/js/pages/datatables.init.js') }}"></script>
+<!-- truy van database -->
+<script>
+    $('#product-table').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: "{{ route('san-pham.lay-danh-sach') }}",
+        order: [],
+        columns: [{
+            data: 'id',
+            name: 'ID Sản phẩm'
+        }, {
+            data: 'ten_sp',
+            name: 'Tên sản phẩm'
+        }, {
+            data: 'mo_ta_sp',
+            name: 'Mô tả sản phẩm'
+        }, {
+            data: 'loai_san_pham.ten_loai_sp',
+            name: 'Loại sản phẩm'
+        }, {
+            data: 'action',
+            name: 'action'
+        }]
+    })
+</script>
 
 @endsection
 
 @section('main-content')
 <div class="row">
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <h4>Danh sách sản phẩm</h4>
-                                <a href="{{ route('san-pham.create') }}" class="btn btn-primary waves-effect waves-light"><i class="fe-plus-circle"></i></a>
-                                <p></p>
-                                <table id="product-table" class="table dt-responsive nowrap">
-                                    <thead>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Tên sản phẩm</th>
-                                            <th>Mô tả sản phẩm</th>
-                                            <th>Loại sản phẩm</th>
-                                            <th>Hành động</th>
-                                        </tr>
-                                    </thead>                              
-                                </table>
-                            </div> <!-- end card body-->
-                        </div> <!-- end card -->
-                    </div><!-- end col-->
-                </div>
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <h4>Danh sách sản phẩm</h4>
+                <a href="{{ route('san-pham.create') }}" class="btn btn-primary waves-effect waves-light">
+                    <span class="btn-label"><i class="fe-plus-circle"></i>
+                    </span>Thêm mới</a>
+                <p></p>
+                <table id="product-table" class="table dt-responsive nowrap">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Tên sản phẩm</th>
+                            <th>Mô tả sản phẩm</th>
+                            <th>Loại sản phẩm</th>
+                            <th>Thao tác</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div> <!-- end card body-->
+        </div> <!-- end card -->
+    </div><!-- end col-->
+</div>
 @endsection
