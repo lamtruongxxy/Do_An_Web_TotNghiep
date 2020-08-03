7047a6824365b3996b99d6b9760ed3690cfc0145
anhtu04091999 <55349397+anhtu04091999@users.noreply.github.com>
2020-08-03 14:46:37 +0700

    Update chi-tiet-don-hang

--- do-an-tot-nghiep/resources/views/DonHang/chi-tiet-don-hang.blade.php
+++ do-an-tot-nghiep/resources/views/DonHang/chi-tiet-don-hang.blade.php
@@ -9,102 +9,85 @@
             <div class="form-row">
                 <div class="col-md-12">
                     <div class="form-row">
-                        <div class="col-md-8">
+                        <div class="col-md-7">
                             <div class="card">
                                 <div class="card-body">
-                                    <h4 class="mb-2 header-title">Chi tiết hóa đơn</h4>
-                                    
+                                    <h4 class="mb-3 header-title">Chi tiết hóa đơn</h4>
+                                    @foreach ($chiTietDonHang as $donhang)
                                     <div class="form-row col-md-12">
-                                        <p><strong>Sẩn phẩm :</strong>&nbsp;I PHONE 5</p>
+                                        <p><strong>Sẩn phẩm:</strong>&nbsp;{{ $donhang->sanPham->ten_sp}}</p>
                                         &nbsp;&nbsp;&nbsp;&nbsp;
-                                        <p><strong>Số lượng :</strong>&nbsp;x3</p>
+                                        <p><strong>Số lượng:</strong>&nbsp;x{{ $donhang->so_luong}}</p>
                                         &nbsp;&nbsp;&nbsp;&nbsp;
-                                        <p><strong>Đơn giá :</strong>&nbsp;{{ $chiTietDonHang }}</p>
+                                        <p><strong>Đơn giá:</strong>&nbsp;
+                                            {{ $donhang->don_gia}}
+                                        </p>
                                     </div>
+                                    @endforeach
                                 </div>
                             </div>
-                            <div class="col-md-12">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="form-group col-md-12">
-                                            <p><strong>Tổng tiền :</strong>&nbsp;<span class="btn btn-danger btn-rounded waves-effect waves-light" data-plugin="counterup">{{ $chiTiet->tong_tien }}</span>&nbsp;<strong>VNĐ</strong></p>
-                                            @if ($chiTiet->trang_thai === 0)
-                                            <strong>Phương thức thanh toán:</strong>&nbsp;<span class="badge badge-primary">Ship COD</span></p>
-                                            @else
-                                            <strong>Phương thức thanh toán:</strong>&nbsp;<span class="badge badge-success">Tại cửa hàng</span></p>
-                                            @endif
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="form-group col-md-12">
-                                            <h4 class="mb-1 header-title">Ghi chú</h4>
-                                            <input type="text" class="form-control" disabled id="ghi_chu" name="ghi_chu" value="{{ $chiTiet->ghi_chu }}">
-                                        </div>
-                                        <div class="form-group col-md-12">
-                                            <strong>Trạng thái: </strong>&nbsp;
-                                            @if ($chiTiet->trang_thai === 0)
-                                            <span class="badge badge-danger">Đã hủy / Tạm xóa</span>
-                                            @elseif ($chiTiet->trang_thai === 1)
-                                            <span class="badge badge-primary">Chờ xác nhận</span>
-                                            @elseif ($chiTiet->trang_thai === 2)
-                                            <span class="badge badge-info">Đã xác nhận</span>
-                                            @else
-                                            <span class="badge badge-success">Hoàn thành</span>
-                                            @endif
-                                        </div>
+
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="form-group col-md-12">
+                                        <h5><strong>Tổng tiền :&nbsp;<span class="text-danger my-4 text-center" data-plugin="counterup">{{ $chiTiet->tong_tien }}</span>&nbsp;VNĐ</strong></h5>
+                                        </br>
+                                        @if ($chiTiet->trang_thai === 0)
+                                        <strong>Phương thức thanh toán:</strong>&nbsp;<span class="badge badge-primary">Ship COD</span></p>
+                                        @else
+                                        <strong>Phương thức thanh toán:</strong>&nbsp;<span class="badge badge-success">Tại cửa hàng</span></p>
+                                        @endif
+                                        <strong>Trạng thái: </strong>&nbsp;
+                                        @if ($chiTiet->trang_thai === 0)
+                                        <span class="badge badge-danger">Đã hủy / Tạm xóa</span>
+                                        @elseif ($chiTiet->trang_thai === 1)
+                                        <span class="badge badge-primary">Chờ xác nhận</span>
+                                        @elseif ($chiTiet->trang_thai === 2)
+                                        <span class="badge badge-info">Đã xác nhận</span>
+                                        @else
+                                        <span class="badge badge-success">Hoàn thành</span>
+                                        @endif
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-5">
                             <div class="form-row">
                                 <div class="col-md-12">
                                     <div class="card">
                                         <div class="card-body">
-                                            <h4 class="mb-1 header-title">Thông tin khách hàng</h4>
+                                            <h4 class="mb-3 header-title">Thông tin khách hàng</h4>
                                             <div class="form-group col-md-12">
-                                                <label for="inputEmail4" class="col-form-label">ID</label>
-                                                <input type="text" class="form-control" disabled id="ten_khach_hang" name="ten_khach_hang" value="{{ $chiTiet->khachHang->id }}">
+                                                <p><strong>Mã ID:</strong>&nbsp;{{ $chiTiet->khachHang->id }}</p>
                                             </div>
                                             <div class="form-group col-md-12">
-                                                <label for="inputEmail4" class="col-form-label">Tên khách hàng</label>
-                                                <input type="text" class="form-control" disabled id="ten_khach_hang" name="ten_khach_hang" value="{{ $chiTiet->khachHang->ten_khach_hang }}">
+                                                <p><strong>Tên khách hàng:</strong>&nbsp;{{ $chiTiet->khachHang->ten_khach_hang }}</p>
                                             </div>
                                             <div class="form-group col-md-12">
-                                                <label for="id">Địa chỉ</label>
-                                                <textarea type="text" class="form-control" disabled id="dia_chi" rows="2" name="dia_chi">"{{ $chiTiet->khachHang->dia_chi }}"</textarea>
+                                                <p><strong>Địa chỉ:</strong>&nbsp;{{ $chiTiet->khachHang->dia_chi }}</p>
                                             </div>
                                             <div class="form-group col-md-12">
-                                                <label for="inputEmail4" class="col-form-label">SĐT</label>
-                                                <input type="text" class="form-control" disabled id="sdt" name="sdt" value="{{ $chiTiet->khachHang->sdt }}">
+                                                <p><strong>SĐT:</strong>&nbsp;{{ $chiTiet->khachHang->sdt }}</p>
                                             </div>
                                             <div class="form-group col-md-12">
-                                                <label for="inputEmail4" class="col-form-label">Email</label>
-                                                <input type="text" class="form-control" disabled id="email" name="email" value="{{ $chiTiet->khachHang->email }}">
+                                                <p><strong>Email:</strong>&nbsp;{{ $chiTiet->khachHang->email }}</p>
                                             </div>
+                                        </div>
+                                    </div>
+                                    <div class="card">
+                                        <div class="card-body">
                                             <div class="form-group col-md-12">
+                                                <p><strong>Ghi chú</strong></br>{{ $chiTiet->ghi_chu }}</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </form>
-    </div>
-    <div class="form-group col-md-12">
-        <div class="card">
-            <div class="card-body">
-                <a href="{{ route('don-hang.danh-sach') }}" class="btn btn-primary waves-effect waves-light"><i class="fas fa-fast-backward"></i></a>
-            </div>
-        </div>
     </div>
 </div>
 </form>
