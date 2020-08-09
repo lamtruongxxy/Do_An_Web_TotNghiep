f99083849c7cc31bf9689c2db21127e53fd97e70
anhtu04091999 <55349397+anhtu04091999@users.noreply.github.com>
2020-08-09 14:21:36 +0700

    edit icon admin

--- do-an-tot-nghiep/resources/views/Components/topbar-start.blade.php
+++ do-an-tot-nghiep/resources/views/Components/topbar-start.blade.php
@@ -43,7 +43,7 @@
             <li class="dropdown notification-list">
                 <a class="nav-link dropdown-toggle nav-user mr-0 waves-effect" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
 
-                    <img src="{{ asset('assets/images/users/user-1.jpg') }}" alt="user-image" class="rounded-circle">
+                    <img src="{{ asset('assets/images/users/user.png') }}" alt="user-image" class="rounded-circle">
                     <span class="pro-user-name ml-1">
                         {{ Auth()->user()->ho_ten }}
                         <i class="mdi mdi-chevron-down"></i>
