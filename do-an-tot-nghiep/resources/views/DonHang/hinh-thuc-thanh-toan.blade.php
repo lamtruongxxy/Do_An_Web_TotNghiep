ebbcc97d3cb971e55339f2aa8e068feed2bf4f27
anhtu04091999 <55349397+anhtu04091999@users.noreply.github.com>
2020-08-12 08:35:16 +0700

    Update hinh-thuc-thanh-toan.blade.php

--- do-an-tot-nghiep/resources/views/DonHang/hinh-thuc-thanh-toan.blade.php
+++ do-an-tot-nghiep/resources/views/DonHang/hinh-thuc-thanh-toan.blade.php
@@ -1,5 +1,5 @@
 @if ($data->trang_thai === 0)
 <span class="badge badge-primary">Ship COD</span>
 @else
-<span class="badge badge-success">Tại cửa hàng</span>
+<span class="badge badge-success">Chuyển khoản</span>
 @endif
