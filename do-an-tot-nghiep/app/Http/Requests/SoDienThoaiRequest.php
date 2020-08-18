0e47b78f4987c1fe47c30d994c96fbdef359d69a
anhtu04091999 <55349397+anhtu04091999@users.noreply.github.com>
2020-08-18 09:23:15 +0700

    create SoDienThoaiRequest

---
+++ do-an-tot-nghiep/app/Http/Requests/SoDienThoaiRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SoDienThoaiRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'sdt' => 'required|regex:/(0)[1-9]{1}[0-9]{8}/',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'sdt.required' => 'Sdt bị trống',
+            'sdt.regex' => 'Sdt không hợp lệ',
+        ];
+    }
+}
